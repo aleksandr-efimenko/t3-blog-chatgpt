40500dcfad3d801c7b5e115986db62ef083e4510
androidblog <androidblog@yandex.ru>
2023-04-27 13:17:32 +0400

    get predictions from openai

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -2,9 +2,8 @@ import { z } from "zod";
 
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-    organization: process.env.OPENAI_ORG,
-    apiKey: process.env.OPENAI_API_KEY,
-
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -16,33 +15,44 @@ import {
 
 export const openAiRouter = createTRPCRouter({
   getOpenAiModels: publicProcedure.query(async () => {
-    const response = await openai.listModels()
-      .then((response) => {
-        console.log(response);
-        return response;
-      })
+    const response = await openai.listModels().then((response) => {
+      console.log(response);
+      return response;
+    });
 
     console.log(response);
-    return 'response';
+    return "response";
   }),
   generateCompletion: publicProcedure
-  .input(z.object({ text: z.string() }))
-  .mutation(async ({ input }) => {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: input.text,
-      max_tokens: 1000,
-      temperature: 0,
-    }).then((response) => {
-      console.log(response);
+    .input(z.object({ text: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const response = await openai
+        .createCompletion({
+          model: "text-davinci-003",
+          prompt: input.text,
+          max_tokens: 2000,
+          temperature: 0.7,
+        })
+        .then((response) => {
+          console.log(response);
 
-      return {
-        response: response?.data?.choices?.[0]?.text || 'No response'
-      }
-    })
-    return response;
-  }),
-  
+          return {
+            response: response?.data?.choices?.[0]?.text || "No response",
+          };
+        });
+
+      // generate query to save to db with prisma
+      const query = await ctx.prisma.post.create({
+        data: {
+          title: input.text,
+          content: response.response,
+          published: true,
+        },
+      });
+
+      return response;
+    }),
+
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -52,7 +62,17 @@ export const openAiRouter = createTRPCRouter({
     }),
 
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+    return ctx.prisma.post.findMany();
+  }),
+
+  createPost: publicProcedure.mutation(async ({ ctx, input }) => {
+     await ctx.prisma.post.create({
+      data: {
+        title: input.title,
+        content: input.content,
+        published: input.published,
+      },
+    });
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
