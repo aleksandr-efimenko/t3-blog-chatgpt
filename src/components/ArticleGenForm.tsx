02fc9590287016e501c44dba357b0626d5a99013
androidblog <androidblog@yandex.ru>
2023-04-28 20:42:12 +0400

    create article generation form separated from page

---
+++ src/components/ArticleGenForm.tsx
@@ -0,0 +1,58 @@
+import React, { useCallback, useState } from "react";
+import { api } from "~/utils/api";
+import Button from "./Button";
+import dynamic from "next/dynamic";
+import { OpenAiSettingsProps } from "./OpenAiSettings";
+
+const SimpleMdeEditor = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+export default function ArticleGenForm({
+  title,
+  setTitle,
+  content,
+  setContent,
+  openAIFetchingStatus,
+  generatePost,
+}: {
+  title: string;
+  setTitle: (title: string) => void;
+  content: string;
+  setContent: (content: string) => void;
+  openAIFetchingStatus: "idle" | "pending" | "fulfilled" | "rejected";
+  generatePost: (e: React.FormEvent<HTMLFormElement>) => void;
+}) {
+  const onChangeMDE = useCallback(
+    (value: string) => {
+      setContent(value);
+    },
+    [setContent]
+  );
+
+  return (
+    <div>
+      <form onSubmit={generatePost} className="flex flex-1 flex-col">
+        <input
+          className="rounded-input"
+          placeholder="Title"
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
+
+        <Button
+          type="submit"
+          loading={openAIFetchingStatus === "pending"}
+          disabled={!title || openAIFetchingStatus === "pending"}
+        >
+          {openAIFetchingStatus === "pending" ? "In progress" : "Generate"}
+        </Button>
+
+        <div className="mt-3 bg-slate-300">
+          <SimpleMdeEditor value={content} onChange={onChangeMDE} />
+        </div>
+      </form>
+    </div>
+  );
+}
