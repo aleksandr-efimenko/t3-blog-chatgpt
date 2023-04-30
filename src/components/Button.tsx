59e0610e99f5e7c38accc63bcf8576b44d46598c
androidblog <androidblog@yandex.ru>
2023-04-30 16:12:19 +0400

    add status prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import AnimatedSpinner from "./AnimatedSpinner";
 
 interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
-  loading?: boolean;
+  status?: "idle" | "pending" | "fulfilled" | "rejected";
 }
 
 export default function Button(props: ButtonProps) {
   return (
     <button
       {...props}
-      className="inline-flex items-center justify-center rounded-md 
-      bg-indigo-500 px-4 py-2 text-sm font-semibold leading-6 text-white
-       shadow transition duration-150 ease-in-out hover:bg-indigo-400
-       w-full"
+      className="inline-flex w-full items-center justify-center 
+      rounded-md bg-indigo-500 px-4 py-2 text-sm font-semibold leading-6
+       text-white shadow transition duration-150 ease-in-out
+       hover:bg-indigo-400"
     >
-      {props.loading && <AnimatedSpinner />}
+      {props.status === "pending" && <AnimatedSpinner />}
       {props.children}
     </button>
   );
