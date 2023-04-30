012dc872030c611e350e220f1ad4d9a5957f1f31
androidblog <androidblog@yandex.ru>
2023-04-30 19:47:13 +0400

    minor style changes

--- src/components/GenerateImg.tsx
+++ src/components/GenerateImg.tsx
@@ -47,10 +47,10 @@ export default function GenerateImg() {
     </option>
   ));
   return (
-    <div className="flex w-full flex-col text-white">
+    <div className="flex w-full flex-col text-white mb-5">
       <h1 className="text-2xl font-bold">Generate Images</h1>
-      <div className="flex w-full">
-        <form onSubmit={generateImg} className=" w-1/3">
+      <div className="flex w-full gap-5">
+        <form onSubmit={generateImg} className=" w-1/4">
           <input
             type="text"
             className="rounded-input"
@@ -72,9 +72,9 @@ export default function GenerateImg() {
           >
             {sizes}
           </select>
-          <Button type="submit">Generate Images</Button>
+          <Button status={imgGenStatus} type="submit">Generate Images</Button>
         </form>
-        <div className="m-5 grid grid-cols-3 gap-3">
+        <div className="grid grid-cols-3 gap-3 w-3/4">
           {imgGenStatus === "pending" && <AnimatedSpinner />}
           {images.map((image) => (
             <img
